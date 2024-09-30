c778730d12ed2b0aa29c29dac259dd3b31668e61
Jefferson Lima <jbldesenvolvimento@gmail.com>
2024-09-30 17:08:37 -0300

    React Router - Utilizando navegação entre paginas.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// 1 - Config react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// Components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        {/* 2 - Links com react router */} 
+        <Navbar></Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/hoocks/useFetch.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+
+// 4 - Custom hook
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  // 5 - Refatorando post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  // 6 - Loading
+  const [loading, setLoading] = useState(false);
+
+  // 7 - Tratando erros
+  const [error, setError] = useState(null);
+
+  // Tarefa - Deletar um registro no JSON
+  const [itemId, setItemId] = useState(null);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod(method);
+    } else if (method === "DELETE") {
+      setConfig({
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      setMethod(method);
+      setItemId(data);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // 9 - loading
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+
+        const json = await res.json();
+
+        setData(json);
+      } catch (error) {
+        setError("Houve algum erro ao carregar os dados!");
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [url, callFetch]);
+
+  // 5 - Refatorando post
+  useEffect(() => {
+    let json;
+
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        json = await res.json();
+      } else if (method === "DELETE") {
+        const deleteUrl = `${url}/${itemId}`;
+
+        const res = await fetch(deleteUrl, config);
+
+        json = await res.json();
+      }
+      setCallFetch(json);
+    };
+    httpRequest();
+  }, [config, method, url]);
+
+  return { data, httpConfig, loading, error };
+};
