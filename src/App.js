7ab8f5ed087c227be662500fd928ab59656ccd93
Jefferson Lima <jbldesenvolvimento@gmail.com>
2024-10-01 17:40:50 -0300

    4 - Carregamento de dados ou Rota Dinamica,  6 - nested route

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import "./App.css";
 // 1 - Config react router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Components
+import Navbar from "./components/Navbar";
+
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
-
-// Components
-import Navbar from "./components/Navbar";
+import Product from "./pages/Product";
+import Info from "./pages/Info";
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
-        {/* 2 - Links com react router */} 
+        {/* 2 - Links com react router */}
         <Navbar></Navbar>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* 4 - Carregamento de dados ou Rota Dinamica */}
+          <Route path="/products/:id" element={<Product />} />
+          {/* 6 - nested route */}
+          <Route path="/products/:id/info" element={<Info />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Product.js
@@ -0,0 +1,29 @@
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../hoocks/useFetch";
+
+const Product = () => {
+  // 4 - Carregamento de dados ou Rota Dinamica
+  const { id, name } = useParams();
+
+  // 5 - Carregamento dado individual
+  const url = "http://localhost:3000/products/" + id;
+  const { data: product, loading, error } = useFetch(url);
+
+  return (
+    <>
+      <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R$ {product.price}</p>
+          {/* 6 - Nested routes */}
+          <Link to={`/products/${product.id}/info`}> Mais informações</Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Product;
