0bbe6bbc972ec2951fb38d5f7c3fa1d58dadaf46
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-10-20 16:13:15 -0300

    React Router - Principais recursos da Lib

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
+import SearchForm from "./components/SearchForm";
+import Search from "./pages/Search";
+import { Navigate } from "react-router-dom/dist";
 
 function App() {
   return (
@@ -18,15 +22,22 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         {/* 2 - Links com react router */}
-        <Navbar></Navbar>
-
+        <Navbar />
+        {/* 9 - Search */}
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* 4 - Carregamento de dados ou Rota Dinamica */}
-          <Route path="/products/:id" element={<Product />} />
           {/* 6 - nested route */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 4 - Carregamento de dados ou Rota Dinamica */}
+          <Route path="/products/:id" element={<Product />} />
+          {/* 9 - Search */}
+          <Route path="/search" element={<Search />} />
+          {/* 10 - Redirect */}
+          <Route path="/company" element={<Navigate to="about" />} />
+          {/* 7 - no match route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>
+        404
+        <p>Não há nada aqui =)</p>
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
